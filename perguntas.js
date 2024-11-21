16b0fca6dbf774f74ffa477e64b5458df6da206d
Faria3A <veigakawan7@gmail.com>
2024-11-21 16:55:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Disney',
+    'Qual é o nome do cachorro do Mickey Mouse?',
+    'Pluto'
+)
+
+criaCartao(
+    'Disney',
+    'Qual é a cor do vestido da cinderela durante o baile no castelo?',
+    'prata'
+)
+
+criaCartao(
+    'Disney',
+    'Em o Rei Leão, qual é o nome do pai de simba?',
+    'Mufasa'
+)
+
+criaCartao(
+    'Disney',
+    'Qual princesa da Disney possui uma lâmpada mágica no filme Aladdin?',
+    'Jasmine'
+)
